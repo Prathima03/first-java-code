f5e9297d03010d6446e9094c7fe62eb5d16587ce
Prathima03 <gollapudiprathima@gmail.com>
2020-09-11 16:03:52 -0400

    my first java code

---
+++ src/com/prathimacode/Main.java
@@ -0,0 +1,10 @@
+package com.prathimacode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("ManualTesting");
+        System.out.println("AutomationTesting");
+    }
+}
